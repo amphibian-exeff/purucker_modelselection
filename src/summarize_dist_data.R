63bcbc32b0337d69894bb1ae916e104d0f975194
epaulukonis <elizabethpaulukonis@gmail.com>
2020-09-25 12:42:42 -0400

    code for getting mean, sd by source, species and chemical

---
+++ src/summarize_dist_data.R
@@ -0,0 +1,28 @@
+
+#code that summarizes the distribution of empirical data based on all combinations of 
+#species x chemical x publication
+#assume normal distribution, return the mean and sd
+
+lab <- read.csv("data_in/updated_amphib_dermal_collated.csv")
+lab<-lab[,c(2:13)]
+levels(lab$source) #10 sources
+#species x chemical x publication
+#split by source
+
+by_s<-split(lab, lab$source) 
+  out<-lapply(by_s,function(x){
+    with(x, aggregate(tissue_conc_ugg ~ species + chemical, 
+                      FUN =  function(y) c( MN= mean(y), SD = sd(y) ))) 
+  })
+  
+out
+out<-do.call(rbind.data.frame,out) #convert to workable df
+out$source<-rownames(out)
+
+#fix nested dimensions within output, final output
+fin_list<-do.call(data.frame, out)
+
+
+
+
+ 
